ad28b5e878f6d2e5736152a6ce3f31d9cd0516f8
Yuri Silva <yrgamerbr@gmail.com>
2024-09-13 13:56:37 -0300

    homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,29 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Pretendard", ...defaultTheme.fontFamily.sans],
+                title: ["Reed"],
+            },
+            colors: {
+                brand: {
+                    white: "var(--white)",
+                    black: "var(--black)",
+                    "real-white": "var(--real-white)",
+
+                    primary: "var(--primary)",
+                    "primary-dark": "var(--primary-dark)",
+                    muted: "var(--muted)",
+                    "neutral-muted": "var(--neutral-muted)",
+                },
+            },
+        },
+    },
+    plugins: [],
 } satisfies Config;
 
 export default config;
