ce4a442904ad211056232499c393dfeaea35c6a8
Yuri Silva <yrgamerbr@gmail.com>
2024-09-17 16:00:14 -0300

    webm is priority, i may add mp3 later

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -3,3 +3,29 @@ export function formatDuration(seconds: number): string {
     const remainingSeconds = Math.floor(seconds % 60);
     return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
 }
+
+function forceDownload(blob: string, filename: string) {
+    var a = document.createElement("a");
+    a.download = filename;
+    a.href = blob;
+    // For Firefox https://stackoverflow.com/a/32226068
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+}
+
+// Current blob size limit is around 500MB for browsers
+export function downloadResource(url: string, filename: string) {
+    fetch(url, {
+        headers: new Headers({
+            Origin: location.origin,
+        }),
+        mode: "cors",
+    })
+        .then((response) => response.blob())
+        .then((blob) => {
+            let blobUrl = window.URL.createObjectURL(blob);
+            forceDownload(blobUrl, filename);
+        })
+        .catch((e) => console.error(e));
+}
